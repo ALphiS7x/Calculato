5d7b4d25bc16c86b81a669c64cec7e231ffd737a
ALphiS7x <vladpietrashko@mail.ru>
2022-12-26 01:09:29 +0300

    Update Script.js

--- src/Script/Script.js
+++ src/Script/Script.js
@@ -23,17 +23,17 @@ export const Script = () => {
             <div className='tap'>
                 <input className='inp' type='text' placeholder='0' value={clik}></input>
                 <div className='grid'>
-                    <input type='button' value={'9'} className="button" onClick={clikk}></input>
-                    <input type='button' value={'8'} className="button" onClick={clikk}></input>
                     <input type='button' value={'7'} className="button" onClick={clikk}></input>
+                    <input type='button' value={'8'} className="button" onClick={clikk}></input>
+                    <input type='button' value={'9'} className="button" onClick={clikk}></input>
                     <input type='button' value={'/'} className="button2" onClick={clikk}></input>
-                    <input type='button' value={'6'} className="button" onClick={clikk}></input>
-                    <input type='button' value={'5'} className="button" onClick={clikk}></input>
                     <input type='button' value={'4'} className="button" onClick={clikk}></input>
+                    <input type='button' value={'5'} className="button" onClick={clikk}></input>
+                    <input type='button' value={'6'} className="button" onClick={clikk}></input>
                     <input type='button' value={'*'} className="button2" onClick={clikk}></input>
-                    <input type='button' value={'3'} className="button" onClick={clikk}></input>
-                    <input type='button' value={'2'} className="button" onClick={clikk}></input>
                     <input type='button' value={'1'} className="button" onClick={clikk}></input>
+                    <input type='button' value={'2'} className="button" onClick={clikk}></input>
+                    <input type='button' value={'3'} className="button" onClick={clikk}></input>
                     <input type='button' value={'-'} className="button2" onClick={clikk}></input>
                     <input type='button' value={'%'} className="button" onClick={clikk}></input>
                     <input type='button' value={'0'} className="button" onClick={clikk}></input>
